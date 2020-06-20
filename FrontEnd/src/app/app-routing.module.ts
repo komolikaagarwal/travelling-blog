3a189a5aa13d7d1548b74d0b09654e91d010e3f6
komolika Agarwal <kamolikaagarwal1500@gmail.com>
2020-06-20 18:38:45 +0530

    Initail Commit

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { AboutComponent } from './about/about.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { ServicesComponent } from './services/services.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:IndexComponent},
+  { path: 'about', component:AboutComponent },
+  { path: 'rooms', component: RoomsComponent },
+  { path: 'pages', component:ServicesComponent },
+  { path: 'news', component:BlogComponent},
+  { path: 'contact', component:ContactComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { AboutComponent } from './about/about.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    AboutComponent,
+    RoomsComponent,
+    BlogComponent,
+    ContactComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
