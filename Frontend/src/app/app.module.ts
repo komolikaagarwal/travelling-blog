e2356a6134c77c310563de947d3ba9ef7bab45a1
komolika Agarwal <kamolikaagarwal1500@gmail.com>
2020-07-02 11:49:24 +0530

    Initail Commit

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,27 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+
 import { ListingComponent } from './listing/listing.component';
 import { BlogComponent } from './blog/blog.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { ListingDetailsComponent } from './listing-details/listing-details.component';
-import { BlogDetailsComponent } from './blog-details/blog-details.component';
-import { LoginComponent } from './login/login.component';
-import { RegistrationComponent } from './registration/registration.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    
     ListingComponent,
     BlogComponent,
-    AboutComponent,
-    ContactComponent,
-    ListingDetailsComponent,
-    BlogDetailsComponent,
-    LoginComponent,
-    RegistrationComponent
+   
   ],
   imports: [
     BrowserModule,
